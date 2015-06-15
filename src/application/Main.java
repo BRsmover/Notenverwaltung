723f447e7b36fc0803e45a9483be5d59f5296669
BRsmover <benjamin.jenni@secacon.com>
2015-06-15 13:17:51 +0200

    Rearranged GUI and added Adding.fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -33,5 +34,25 @@ public class Main extends Application {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public void callAdding() {
+		try {
+			// Load Adding.fxml
+			FXMLLoader loadAdding = new FXMLLoader();
+			loadAdding.setLocation(Main.class.getResource("Adding.fxml"));
+			BorderPane addLayout = (BorderPane) loadAdding.load();
+
+			Scene addScene = new Scene(addLayout);
+			Stage addStage = new Stage();
+			addStage.setScene(addScene);
+			addStage.show();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+		Platform.exit();
 	}
 }
