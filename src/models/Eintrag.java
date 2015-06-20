24e3377c1a23493c11811a5ccf179f2be8b337de
BRsmover <benjamin.jenni@secacon.com>
2015-06-20 10:17:58 +0200

    Redesigned the data writing/reading and added the table view Redesigned the saving

--- src/models/Eintrag.java
+++ src/models/Eintrag.java
@@ -1,76 +1,111 @@
 package models;
 
+import java.io.Serializable;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 
 /**
  * 
  * @author Benjamin Jenni
  *
  */
-public class Eintrag {
-	private String vorname;
-	private String name;
-	private String fach;
-	private double noteEins;
-	private double noteZwei;
-	private double noteDrei;
+public class Eintrag implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private StringProperty vorname;
+	private StringProperty name;
+	private StringProperty fach;
+	private DoubleProperty noteEins;
+	private DoubleProperty noteZwei;
+	private DoubleProperty noteDrei;
 
 	public Eintrag() {
 	}
 
 	public Eintrag(String vorname, String name, String fach, double noteEins, double noteZwei, double noteDrei) {
-		this.vorname = vorname;
-		this.name = name;
-		this.fach = fach;
-		this.noteEins = noteEins;
-		this.noteZwei = noteZwei;
-		this.noteDrei = noteDrei;
+		this.vorname = new SimpleStringProperty(vorname);
+		this.name = new SimpleStringProperty(name);
+		this.fach = new SimpleStringProperty(fach);
+		this.noteEins = new SimpleDoubleProperty(noteEins);
+		this.noteZwei = new SimpleDoubleProperty(noteZwei);
+		this.noteDrei = new SimpleDoubleProperty(noteDrei);
 	}
 
-	public double getNoteEins() {
+	public DoubleProperty getNoteEinsProperty() {
 		return noteEins;
 	}
 
-	public void setNoteEins(double noteEins) {
-		this.noteEins = noteEins;
+	public double getNoteEins() {
+		return noteEins.get();
 	}
 
-	public double getNoteZwei() {
+	public void setNoteEins(double noteEins) {
+		this.noteEins = new SimpleDoubleProperty(noteEins);
+	}
+
+	public DoubleProperty getNoteZweiProperty() {
 		return noteZwei;
 	}
 
-	public void setNoteZwei(double noteZwei) {
-		this.noteZwei = noteZwei;
+	public double getNoteZwei() {
+		return noteZwei.get();
 	}
 
-	public double getNoteDrei() {
+	public void setNoteZwei(double noteZwei) {
+		this.noteZwei = new SimpleDoubleProperty(noteZwei);
+	}
+
+	public DoubleProperty getNoteDreiProperty() {
 		return noteDrei;
 	}
 
-	public void setNoteDrei(double noteDrei) {
-		this.noteDrei = noteDrei;
+	public double getNoteDrei() {
+		return noteDrei.get();
 	}
 
-	public String getVorname() {
+	public void setNoteDrei(double noteDrei) {
+		this.noteDrei = new SimpleDoubleProperty(noteDrei);
+	}
+
+	public StringProperty getVornameProperty() {
 		return vorname;
 	}
 
-	public void setVorname(String vorname) {
-		this.vorname = vorname;
+	public String getVorname() {
+		return vorname.get();
 	}
 
-	public String getName() {
+	public void setVorname(String vorname) {
+		this.vorname = new SimpleStringProperty(vorname);
+	}
+
+	public StringProperty getNameProperty() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public String getName() {
+		return name.get();
 	}
 
-	public String getFach() {
+	public void setName(String name) {
+		this.name = new SimpleStringProperty(name);
+	}
+
+	public StringProperty getFachProperty() {
 		return fach;
 	}
 
+	public String getFach() {
+		return fach.get();
+	}
+
 	public void setFach(String fach) {
-		this.fach = fach;
+		this.fach = new SimpleStringProperty(fach);
 	}
 }
